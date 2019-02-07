b6536a5b848e47e8166bbe7ce67e2540828cb8eb
simone.rainieri <simone.rainieri@gmail.com>
2019-02-07 18:33:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
+ 
+def countdown(n)
+while n > 1 
+puts "#{n} SECOND(S)!" #write your code here
+n -= 1
+return "HAPPY NEW YEAR!"
+end
+end
 
-def countdown
+def countdown_with_sleep(n)
+  while n > 1 
+puts "#{n} SECOND(S)!" #write your code here
+n -= 1
+return "HAPPY NEW YEAR!"
+end
 end
